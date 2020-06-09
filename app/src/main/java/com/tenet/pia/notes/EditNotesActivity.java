7c005140d8461f47a30ba133419c4058c38091bc
108X <42265965+108X@users.noreply.github.com>
2020-06-09 10:55:00 +0800

    新建文件

---
+++ app/src/main/java/com/tenet/pia/notes/EditNotesActivity.java
@@ -0,0 +1,16 @@
+package com.tenet.pia.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tenet.pia.R;
+
+public class EditNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_notes);
+    }
+}
