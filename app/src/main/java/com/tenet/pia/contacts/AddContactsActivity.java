d1cca80c8d58754abdaf2720cc917cfcc3a0c521
Cheng <2593673096@qq.com>
2020-06-12 20:20:50 +0800

    编写页面

--- app/src/main/java/com/tenet/pia/contacts/AddContactsActivity.java
+++ app/src/main/java/com/tenet/pia/contacts/AddContactsActivity.java
@@ -2,19 +2,15 @@ package com.tenet.pia.contacts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
 
 import com.tenet.pia.R;
 
-public class EditContactsActivity extends Activity {
+public class AddContactsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_contacts);
-
-
+        setContentView(R.layout.activity_add_contacts);
     }
 }
