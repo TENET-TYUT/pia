9c45e59cd886ff3c892d85d763ee7541d3f49e9b
drb1122 <66583694+drb1122@users.noreply.github.com>
2020-06-13 14:15:19 +0800

    编写页面

--- app/src/main/java/com/tenet/pia/contacts/ContactsInfoActivity.java
+++ app/src/main/java/com/tenet/pia/contacts/ContactsInfoActivity.java
@@ -1,14 +1,11 @@
 package com.tenet.pia.contacts;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.tenet.pia.R;
 
 public class ContactsInfoActivity extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts_info);
