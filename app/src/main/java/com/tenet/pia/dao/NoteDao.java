8719d9fc4c2385c915f935ed6e03f9727642b1df
drb1122 <66583694+drb1122@users.noreply.github.com>
2020-06-14 22:32:18 +0800

    dao

--- app/src/main/java/com/tenet/pia/dao/NoteDao.java
+++ app/src/main/java/com/tenet/pia/dao/NoteDao.java
@@ -1,3 +1,55 @@
+package com.tenet.pia.dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.tenet.pia.entity.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteDao {
+    private static final String NOTE_TITLE = "noteTitle";
+    private static final String NOTE_CONTENT = "noteContent";
+    private static final String CREATE_TIME = "createTime";
+    private DateBaseHelper dateBaseHelper;
+    private ArrayList<Note> noteList;
+
+    public NoteDao(Context context) {
+        dateBaseHelper = DateBaseHelper.getInstance(context);
+    }
+
+    public void insert(Note note) {
+        SQLiteDatabase db = dateBaseHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(NOTE_TITLE, note.getNoteTitle());
+        values.put(NOTE_CONTENT, note.getNoteContent());
+        values.put(NOTE_CONTENT, note.getNoteContent());
+        long id = db.insert(DateBaseHelper.NOTE_TABLE, null, values);
+        db.close();
+    }
+
+    public void update(Note note){
+        SQLiteDatabase db = dateBaseHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(NOTE_TITLE,note.getNoteTitle());
+        values.put(NOTE_CONTENT, note.getNoteContent());
+        values.put(NOTE_CONTENT, note.getNoteContent());
+        long id=db.update(DateBaseHelper.NOTE_TABLE,values,"name=?",new String[]{})
+    }
+
+    public void delete(Note note){
+
+    }
+
+    public List<Note> query() {
+        noteList = new ArrayList<>();
+        SQLiteDatabase db = dateBaseHelper.getReadableDatabase();
+        Cursor cursor = db.query("NOTE_TABLE", null, null, null, null, null, null);
+        if (cursor.getCount() != 0) {
+            cursor.moveToFirst();
         }
         while (cursor.moveToNext()) {
             String notetitle = cursor.getString(cursor.getColumnIndex(NOTE_TITLE));
