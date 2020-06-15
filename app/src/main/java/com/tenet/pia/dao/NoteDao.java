9ce2317d387f079307bcfebc911592155e818a2a
108X <42265965+108X@users.noreply.github.com>
2020-06-15 09:16:52 +0800

    Update NoteDao.java

--- app/src/main/java/com/tenet/pia/dao/NoteDao.java
+++ app/src/main/java/com/tenet/pia/dao/NoteDao.java
@@ -16,7 +16,6 @@ public class NoteDao {
     private static final String NOTE_CONTENT = "noteContent";
     private static final String CREATE_TIME = "createTime";
     private DateBaseHelper dateBaseHelper;
-    private ArrayList<Note> noteList;
 
     public NoteDao(Context context) {
         dateBaseHelper = DateBaseHelper.getInstance(context);
@@ -33,45 +32,50 @@ public class NoteDao {
         db.close();
     }
 
-    public int update(Note note){
+    public int update(Note note) {
         SQLiteDatabase db = dateBaseHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put(ID, note.getId());
-        values.put(NOTE_TITLE,note.getNoteTitle());
+        values.put(NOTE_TITLE, note.getNoteTitle());
         values.put(NOTE_CONTENT, note.getNoteContent());
         values.put(NOTE_CONTENT, note.getNoteContent());
-        int number=db.update(DateBaseHelper.NOTE_TABLE,values,"id=?",new Integer[]{note.getId()});
+        int number = db.update(DateBaseHelper.NOTE_TABLE, values, ID + "=?", new String[]{String.valueOf(note.getId())});
         db.close();
         return number;
     }
 
     public int delete(Note note) {
         SQLiteDatabase db = dateBaseHelper.getWritableDatabase();
-        int number = db.delete(DateBaseHelper.NOTE_TABLE, "_id=?", new Int[]{note.getId()});
+        int number = db.delete(DateBaseHelper.NOTE_TABLE,  ID + "=?", new String[]{String.valueOf(note.getId())});
         db.close();
         return number;
     }
 
-    public boolean find(long id){
+    public Note find(long id) {
         SQLiteDatabase db = dateBaseHelper.getReadableDatabase();
-        Cursor cursor = db.query(DateBaseHelper.NOTE_TABLE,null,"_id=?",new String[]{},null,null,null);
-        boolean result=cursor.moveToNext();
+        Cursor cursor = db.query(DateBaseHelper.NOTE_TABLE, null,  ID + "=?", new String[]{String.valueOf(id)}, null, null, null);
+        cursor.moveToFirst();
+        long _id = cursor.getLong(cursor.getColumnIndex(ID));
+        String noteTitle = cursor.getString(cursor.getColumnIndex(NOTE_TITLE));
+        String noteContent = cursor.getString(cursor.getColumnIndex(NOTE_CONTENT));
+        long createTime = cursor.getLong(cursor.getColumnIndex(CREATE_TIME));
         cursor.close();
         db.close();
-        return result;
+        return new Note(_id, noteTitle, noteContent, createTime);
     }
+
     public List<Note> query() {
-        noteList = new ArrayList<>();
+        List<Note> noteList = new ArrayList<>();
         SQLiteDatabase db = dateBaseHelper.getReadableDatabase();
-        Cursor cursor = db.query("NOTE_TABLE", null, null, null, null, null, null);
+        Cursor cursor = db.query(DateBaseHelper.NOTE_TABLE, null, null, null, null, null, null);
         if (cursor.getCount() != 0) {
             cursor.moveToFirst();
         }
         while (cursor.moveToNext()) {
-            String notetitle = cursor.getString(cursor.getColumnIndex(NOTE_TITLE));
-            String notecontent = cursor.getString(cursor.getColumnIndex(NOTE_CONTENT));
-            String createtime = cursor.getString(cursor.getColumnIndex(CREATE_TIME));
-            Note note = new Note(notetitle, notecontent,createtime);
+            long id = cursor.getLong(cursor.getColumnIndex(ID));
+            String noteTitle = cursor.getString(cursor.getColumnIndex(NOTE_TITLE));
+            String noteContent = cursor.getString(cursor.getColumnIndex(NOTE_CONTENT));
+            long createTime = cursor.getLong(cursor.getColumnIndex(CREATE_TIME));
+            Note note = new Note(id,noteTitle, noteContent, createTime);
             noteList.add(note);
         }
         db.close();
